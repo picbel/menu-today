8838efcd3292f87f5e5927b990c3266438b9da0f
gimheegon <gon@sirloin.co.kr>
2022-04-13 10:53:19 +0900

    카카오 api 테스트 완료

--- src/main/kotlin/com/api/menutoday/component/RestaurantExplorerKaKaoImpl.kt
+++ src/main/kotlin/com/api/menutoday/component/RestaurantExplorerKaKaoImpl.kt
@@ -1,21 +1,17 @@
 package com.api.menutoday.component
 
 import com.api.menutoday.common.util.http.HttpUtil
-import com.api.menutoday.common.util.http.model.HttpRequest
 import com.api.menutoday.domain.restaurant.Restaurant
 
 
-class RestaurantExplorerNaverImpl(
+class RestaurantExplorerKaKaoImpl(
     private val httpclient: HttpUtil
 ) : RestaurantExplorer {
 
     /*
-    https://developers.naver.com/docs/search/local/
-    네이버 검색 api url
-    그냥 일단 하드코딩
+    https://developers.kakao.com/docs/latest/ko/local/dev-guide#search-by-keyword
+    https://developers.kakao.com/docs/latest/ko/local/dev-guide#search-by-category
      */
-//    private val apiUrl = "https://openapi.naver.com/v1/search/local.json"
-
     private fun urlAssemble(keyWord : String) : String{
         return "https://openapi.naver.com/v1/search/local.json?query=$keyWord&display=10&start=1&sort=random"
     }
---
+++ src/test/kotlin/com/api/menutoday/KaKaoApiTest.kt
@@ -0,0 +1,35 @@
+package com.api.menutoday
+
+import com.api.menutoday.common.util.http.HttpUtilImpl
+import com.api.menutoday.common.util.http.model.HttpRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
+
+class KaKaoApiTest {
+    @Test
+    fun apiCall(){
+        var keyword : String = "치킨"
+        try {
+            keyword = URLEncoder.encode(keyword, "UTF-8");
+        } catch (e : UnsupportedEncodingException) {
+            throw RuntimeException("encoding fail!",e);
+        }
+
+        val request = HttpRequest(
+            url = "https://dapi.kakao.com/v2/local/search/keyword.json?y=37.514322572335935&x=127.06283102249932&radius=20000&query=$keyword"
+        )
+            .addHeader("Authorization","KakaoAK 494febc0331b78332c4b75a58bd2d8a5")
+
+
+        val http = HttpUtilImpl(ObjectMapper())
+        val response = http.get(request)
+
+
+        println("카카오에서 받은 결과 = " + response);
+        System.out.println("-----------------------------------------");
+
+    }
+
+}
