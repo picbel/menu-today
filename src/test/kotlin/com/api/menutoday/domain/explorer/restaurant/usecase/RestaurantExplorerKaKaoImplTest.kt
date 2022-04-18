0c5a7b1c117f806b1620b1185f49fdc71ac6886a
gimheegon <gon@sirloin.co.kr>
2022-04-18 11:01:21 +0900

    코틀린도 ParameterResolver를 이용한 테스트가 가능하다.

--- src/test/kotlin/com/api/menutoday/domain/explorer/restaurant/usecase/RestaurantExplorerKaKaoImplTest.kt
+++ src/test/kotlin/com/api/menutoday/domain/explorer/restaurant/usecase/RestaurantExplorerKaKaoImplTest.kt
@@ -1,7 +1,26 @@
 package com.api.menutoday.domain.explorer.restaurant.usecase
 
+import com.api.menutoday.common.util.http.HttpUtil
+import com.api.menutoday.common.util.http.model.HttpRequest
+import com.api.menutoday.domain.explorer.restaurant.fixture.HttpUtilImplWithRestaurantResolver
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(HttpUtilImplWithRestaurantResolver::class)
 class RestaurantExplorerKaKaoImplTest{
 
+    private lateinit var sut : RestaurantExplorerKaKaoImpl
+
+    @BeforeEach
+    fun setUp(httpUtil : HttpUtil){
+        sut = RestaurantExplorerKaKaoImpl(httpUtil, ObjectMapper())
+    }
+
+    @Test
+    fun test(){
+
+    }
 }
